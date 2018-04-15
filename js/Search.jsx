be13717e8a42b63d5c8673bd65c9150dd988ccfe
Fabian Luna <flb031@gmail.com>
2018-04-15 11:13:48 -0500

    arrow functions

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ShowCard from './ShowCard';
 import preload from '../data.json';
 
-const Search = () => (
-  <div className="search">
-    {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
-  </div>
-);
+class Search extends Component {
+  state = {
+    search: ''
+  };
+
+  handleChangeSearch = event => {
+    this.setState({ search: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input
+            type="text"
+            placeholder="Search"
+            value={this.state.search}
+            onChange={this.handleChangeSearch}
+          />
+        </header>
+
+        <div>
+          {preload.shows
+            .filter(
+              show =>
+                `${show.title} ${show.description}`
+                  .toUpperCase()
+                  .indexOf(this.state.search.toUpperCase()) >= 0
+            )
+            .map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
+
+      </div>
+    );
+  }
+}
 
 export default Search;
