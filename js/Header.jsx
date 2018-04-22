f940208b6fc1acc3984ce76e2bf202c90fb9e7f5
Fabian Luna <flb031@gmail.com>
2018-04-22 16:25:01 -0500

    Add header component, display logic

---
+++ js/Header.jsx
@@ -0,0 +1,39 @@
+// @flow
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = (props: { showSearch?: boolean, handleSearchTermChange?: Function, searchTerm?: string }) => {
+  let search;
+
+  if (props.showSearch) {
+    search = (
+      <input onChange={props.handleSearchTermChange} value={props.searchTerm} type="text" placeholder="Search" />
+    );
+  } else {
+    search = (
+      <h2>
+        <Link to="search">
+          back
+        </Link>
+      </h2>
+    );
+  }
+
+  return (
+    <header>
+      <h1>
+        <Link to="/">svideo</Link>
+      </h1>
+      {search}
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  showSearch: false,
+  handleSearchTermChange: function foo() {},
+  searchTerm: ''
+};
+
+export default Header;
